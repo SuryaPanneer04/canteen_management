e2e0a39c0afcc38433f85b2ae9abc2dc10243ec3
Gokul01-maker <gokuloffical01@gmail.com>
2026-09-11 17:08:19 +0530

    updated the stock file

--- store/stock.php
+++ store/stock.php
@@ -76,14 +76,17 @@ if ($search !== '') {
 
     $where[] = "
         (
-            material_code LIKE :search
-            OR material_name LIKE :search
-            OR category LIKE :search
+            material_code LIKE :search_code
+            OR material_name LIKE :search_name
+            OR category LIKE :search_category
         )
     ";
 
-    $params[':search'] = '%' . $search . '%';
+    $searchValue = '%' . $search . '%';
 
+    $params[':search_code'] = $searchValue;
+    $params[':search_name'] = $searchValue;
+    $params[':search_category'] = $searchValue;
 }
 
 
